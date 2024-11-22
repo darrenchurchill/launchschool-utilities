73df9e4c0669d425c47c169a35f2d576cb2adcb9
Darren Churchill <darrenmchurchill@gmail.com>
2024-11-22 15:15:44 -0800

    Use js.configs.recommended instead of eslintrc compat helpers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,23 +5,10 @@
  */
 import globals from "globals";
 import babelParser from "@babel/eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-// https://eslint.org/docs/latest/use/configure/migration-guide#using-eslintrc-configs-in-flat-config
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-});
 
 export default [
-  // TODO: use js.configs.recommended and remove FlatCompat
-  ...compat.extends("eslint:recommended"),
+  js.configs.recommended,
   {
     languageOptions: {
       globals: {
